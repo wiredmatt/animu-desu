98c6f38dc36dff96fb7ecfb6cbe0534f9478d452
Mateo Carriquí <mateocarriqui7@gmail.com>
2021-01-30 16:26:29 -0300

    Rebuilt lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,29 +1,6 @@
+import { BaseAnime, AnimeDetails, Released, AnimeEpisode, RecentlyAddedAnime } from "./types";
 declare type int = number;
-interface BaseAnime {
-    id: string;
-    title: string;
-    image: string;
-}
-interface AnimeDetails extends BaseAnime {
-    type: string;
-    summary: string;
-    released: Released;
-    genres: string[];
-    status: string;
-    totalEpisodes: number;
-    otherName: string;
-}
-interface RecentlyAddedAnime extends BaseAnime {
-    episodeNumber: number;
-}
-interface Released {
-    season: string;
-    year: number;
-}
-interface AnimeEpisode {
-    link: string;
-    quality: string;
-}
+export { BaseAnime, AnimeDetails, Released, AnimeEpisode, RecentlyAddedAnime };
 export declare function getPopular(page: int): Promise<BaseAnime[]>;
 export declare function getAnimeDetails(id: string): Promise<AnimeDetails>;
 export declare function search(word: string, page: int): Promise<BaseAnime[]>;
@@ -31,5 +8,4 @@ export declare function getEpisodeLinks(id: string, episode: int): Promise<Anime
 export declare function searchByGenre(genre: string, page: int): Promise<BaseAnime[]>;
 export declare function getRecentlyAdded(page: int): Promise<RecentlyAddedAnime[]>;
 export declare function getGenreList(): Promise<string[]>;
-export {};
 //# sourceMappingURL=index.d.ts.map
---
+++ lib/types/index.d.ts
@@ -0,0 +1,26 @@
+export interface BaseAnime {
+    id: string;
+    title: string;
+    image: string;
+}
+export interface AnimeDetails extends BaseAnime {
+    type: string;
+    summary: string;
+    released: Released;
+    genres: string[];
+    status: string;
+    totalEpisodes: number;
+    otherName: string;
+}
+export interface RecentlyAddedAnime extends BaseAnime {
+    episodeNumber: number;
+}
+export interface Released {
+    season: string;
+    year: number;
+}
+export interface AnimeEpisode {
+    link: string;
+    quality: string;
+}
+//# sourceMappingURL=index.d.ts.map
